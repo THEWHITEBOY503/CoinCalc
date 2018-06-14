13b573490eae5006b85e0c63034f398d6e9738cd
THEWHITEBOY503 <conner@smith.net>
2018-06-14 11:08:32 -0500

    Made the instructions a bit more clear (i actually forgot the whole part about making the cBANK file help my soul)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ THE BANK AND STORE FUNCTIONS DON'T ACTUALLY BUY OR DO ANYTHING REALLY. THEY'RE J
 /*
  Setting up CoinCalc
 First, create a new project in Visual Studio using C# .NET (Not .NET core)
-If you already have a CoinCalc project, you can skip that step.
+* If you already have a CoinCalc project, you can skip that step.
+Make a new text file called "cBANK.txt" somewhere on your computer and type a 0 in the file and save.
+* If you already have a cBANK file, you can skip that step.
 In every place where you see "cBANK.txt", replace that with the file path to your cBANK.txt file.
 If you are on Mac, move the file into the bin/debug/ folder of the project and leave the name as "cBANK.TXT"
 Compile and run.
@@ -22,7 +24,7 @@ using System.Threading.Tasks;
 using System.IO;
 
 
-//Makes a namespace for our program and gives it a name.
+//Makes a namespace for our program and give it a name.
 namespace CoinCalc
 
 {
